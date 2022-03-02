6ebca81e16982e035e06a2068065dd9c0ee9f918
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-03-01 23:02:59 -0500

    commit tree 8a4270f26a216aa86817609c20d29b015b56c526

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'nice', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'black', 'mean', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'brown', 'nice', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sargent Brown', 10, 'M', 'brown', 'nice', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'black', 'mean', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 8, 'M', 'black', 'mean', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'black', 'nice', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'M', 'brown', 'nice', true);
